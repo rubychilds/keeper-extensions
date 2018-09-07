39a1038816ece745fb80c71ec60fd0eb4c1dfde4
unknown <ruby.childs1@gmail.com>
2018-09-07 16:03:45 +0100

    feat(early-login): now has different views if your signedin or not

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,8 +1,76 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
+'use strict';
+
+/**
+ * Global variables, visible only inside background.js
+ */
+var counter = 0;
+
+/**
+ * Run this code every second
+ */
+setInterval(function () {
+    getAllTabs(function (tabs) {
+        dcl(tabs);
+    });
+    dcl(`${counter++} s since installation/reload`);
+}, 1000);
+
+chrome.storage.local.get('signed_in', function(data) {
+  if (data.signed_in) {
+    chrome.browserAction.setPopup({popup: '../html/popup.html'});
+  } else {
+    chrome.browserAction.setPopup({popup: '../html/popup_sign_in.html'});
+  }
 });
+
+/**
+ * Block nasty requests
+ */
+chrome.webRequest.onBeforeRequest.addListener(function (details) {
+    let parser, domain, domainBlocked;
+    let tabId = details.tabId;
+
+    // Get domain (hostname) from URL
+    parser = document.createElement('a');
+    parser.href = details.url;
+    domain = parser.hostname;
+    dcl(details.type + ' - ' + domain);
+
+    domainBlocked = DOMAINS_FORBIDDEN.indexOf(domain) >= 0;
+
+    if (domainBlocked) {
+        chrome.browserAction.setBadgeText({
+            tabId: tabId,
+            text: 'XXX'
+        });
+        return {cancel: true};
+    } else {
+        return {cancel: false};
+    }
+
+    // Block the request if domain matches no whitelisted domain
+    if (domainBlocked.domainBlocked) {
+      return {cancel: true};
+    } else {
+      return {cancel: false};
+    }
+    },
+    {urls: ['*://*/*']},
+    ['blocking']
+);
+
+/**
+ * Context menu
+ */
+chrome.contextMenus.removeAll();
+chrome.contextMenus.create(
+    {
+        id: 'contextMenu',
+        title: 'Chrome Extension Starter Kit',
+        type: 'normal',
+        contexts: ['image'],
+        onclick: function (info, tab) {
+            chrome.tabs.create({url: 'https://www.google.com/search?&tbm=isch&q=' + encodeURI(info.srcUrl)});
+        }
+    }
+);
--- chrome/js/content.js
+++ chrome/js/content.js
@@ -1,6 +1,2 @@
-// Runs in the context of web pages
-alert("Keeping your data safe")
-
 var firstHref = $("a[href^='http']").eq(0).attr("href");
-console.log("Hello");
 console.log(firstHref);
